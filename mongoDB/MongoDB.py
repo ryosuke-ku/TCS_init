659c1e2cfe80250086ff69f4c40b67706399b948
Kurachi <naist1020@gmail.com>
2019-10-11 23:50:55 +0900

    edit

--- mongoDB/MongoDB.py
+++ mongoDB/MongoDB.py
@@ -16,9 +16,19 @@ class TestMongo(object):
         }
         return self.db.test.insert_one(post)
 
+     def add_clone(self):
+        """データ挿入"""
+        clone1 = "aaaaaa"
+        clone2 = "bbbbbb"
+        post2 = {
+            'clone1': clone1,
+            'clone2': clone2,
+        }
+        return self.db.test.insert_one(post2)        
+
 def main():
     obj = TestMongo()
-    rest = obj.add_one()
+    rest = obj.add_clone()
     print(rest)
 
 if __name__ == '__main__':
