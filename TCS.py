86ed19dc4611140389e3fbf8fa384c4cd84b2d5a
Kurachi <naist1020@gmail.com>
2019-10-17 22:44:19 +0900

    add folder delete function

--- TCS.py
+++ TCS.py
@@ -5,20 +5,34 @@ from bs4 import BeautifulSoup as bs4
 from collections import defaultdict
 import collections
 
-x = input('Input Code → ')
-project_list = ['ant']
-for i in project_list:
-    file = open('NICAD/projects/' + i +'/a.java', 'w')
-    file.write(x)
-    file.close()
+inputcode = []
+file = open('NICAD/projects/utility/a.java', 'w')
+print('↓ Input Code ↓')
+while True:
+    x = input()
+    inputcode.append(x)
+    file.write(x.replace('\n', '') + '\n')
+    if x == "":
+        break
+
+file.close()
+for low in inputcode:
+    print(low)
+
+project_list = ['utility']
+# for i in project_list:
+#     file = open('NICAD/projects/' + i +'/a.java', 'w')
+#     file.write(x)
+#     file.close()
 
 os.chdir('C:Users\\ryosuke-ku\\Desktop\\TCS\\NICAD')
 
 print(os.getcwd())
 
-cp = subprocess.run(['./nicad5','functions','java','projects/ant','default-report'])
+cp = subprocess.run(['./nicad5','functions','java','projects/' + project_list[0] ,'default-report'])
 
 os.chdir('C:Users\\ryosuke-ku\\Desktop\\TCS')
 print(os.getcwd())
 
 cp2 = subprocess.run(['python','scraping.py'])
+
